0aa9585139e16938deafd4519fe874e4cf59763f
sidharth verma <67055758+sidharth1017@users.noreply.github.com>
2020-11-25 19:40:45 +0530

    Update and rename jarvis.py to assistant.py

--- assistant.py
+++ assistant.py
@@ -1,8 +1,8 @@
-import pyttsx3
-import speech_recognition as sr
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition 
 import datetime
-import wikipedia
-import webbrowser
+import wikipedia #pip install wikipedia
+import webbrowser #pip install webbrowser
 import os
 import smtplib
 import random
